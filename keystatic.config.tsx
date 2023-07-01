c64d4194f88920e8207d72ea6d7e36993d62c404
Rajat Das <rajat@Rajats-MacBook-Air.local>
2023-07-01 13:04:21 +0530

    config change

--- keystatic.config.tsx
+++ keystatic.config.tsx
@@ -9,15 +9,15 @@ import {
 import { ComponentBlocks } from "./components/ComponentBlocks";
 
 const storage: LocalConfig["storage"] | GitHubConfig["storage"] =
-  process.env.NODE_ENV !== "development"
-    ? { kind: "local" }
-    : {
-        kind: "github",
-        repo: {
-          owner: process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_OWNER!,
-          name: process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG!,
-        },
-      };
+// process.env.NODE_ENV !== "development"
+// ? { kind: "local" }
+{
+  kind: "github",
+  repo: {
+    owner: process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_OWNER!,
+    name: process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG!,
+  },
+};
 
 export default config({
   storage,
